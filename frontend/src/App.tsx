03ce8d5335585a7732e54e25eb86d33ef8d2450d
Eric Chen <erche3742@gmail.com>
2023-04-02 16:54:43 -0400

    Map

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div
+      className="App"
+      style={{ float: "left", marginLeft: "5vw", marginTop: "5vh" }}
+    >
+      <div>
+        <h1>Welcome back Eric!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Modal.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+function Modal() {
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+}
+
+export default Modal;
